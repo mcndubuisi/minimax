35e293b74bcc0377628c22031eca092dc3c45399
Emmanuel Ndubuisi <psynch45@gmail.com>
2021-03-11 21:27:02 -0600

    Update minimax.cs

--- minimax.cs
+++ minimax.cs
@@ -1,31 +1,107 @@
-// todo: a whole lot lmao
+/****************************************************************************** 
+ * Project 1 - Tic Tac Toe
+ * 
+ * Develop a Tic Tac Toe intelligent agent that players as 'O'.
+ * Author: Emmanuel Ndubuisi
+ * Date: March 11, 2021
+ * 
+ * Compilation and Execution:  https://repl.it/@mcndubuisi/TicTacToe
+ ******************************************************************************/
 
 using System;
+using System.Collections.Generic;
 
-class MainClass 
+class TicTacToe 
 {
   public static char[,] board = new char[3,3]{{'-', '-', '-'}, {'-', '-', '-'}, {'-', '-', '-'}};
-  public static char computer = 'X';
+  public static char computer = 'O';
+  public static char human = 'X';
 
   public static void play()
   {
-    Random rand = new Random();
-    int random_x = rand.Next(0, 3);
-    int random_y = rand.Next(0, 3);
+    int[] coordinates = bestMove();
+    board[coordinates[0], coordinates[1]] = computer;
+    printBoard();
+    if(hasWon()) System.Environment.Exit(1);
+  }
 
-    if(isEmpty(random_x, random_y))
+  // find the best move for agent
+  public static int[] bestMove()
+  {
+    int bestScore = int.MinValue;
+    int[] move = null;
+
+    for (int i = 0; i < 3; i++) 
     {
-      random_x = rand.Next(0, 3);
-      random_y = rand.Next(0, 3);
-      board[random_x,random_y] = computer;
-      printBoard();
+      for (int j = 0; j < 3; j++) 
+      {
+        if (board[i,j] == '-')
+        {
+          board[i,j] = computer;
+          int score = minimax(board, 0, false);
+          board[i,j] = '-';
+          if (score > bestScore)
+          {
+            bestScore = score;
+            move = new int[] { i, j };
+          }
+        }
+      }
     }
-    else
+
+    return move;
+  }
+
+  // minimax algorithm that return best score per move
+  public static int minimax(char[,] board, int depth, bool isMaximizing)
+  {
+    string result = checkWinner();
+    var scores = new Dictionary<string, int>(){
+      {"X", -10}, {"O", 10}, {"tie", 0}
+    };
+	  
+    if (result != null)
     {
-      play();
+      return scores[result];
+    }
+
+    if (isMaximizing)
+    {
+      int bestScore = int.MinValue;
+      for (int i = 0; i < 3; i++)
+      {
+        for (int j = 0; j < 3; j++)
+        {
+          if (board[i,j] == '-')
+          {
+            board[i,j] = computer;
+            int score = minimax(board, depth + 1, false);
+            board[i,j] = '-';
+            bestScore = Math.Max(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    }
+    else 
+    {
+      int bestScore = int.MaxValue;
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          // Is the spot available?
+          if (board[i,j] == '-') {
+            board[i,j] = human;
+            int score = minimax(board, depth + 1, true);
+            board[i,j] = '-';
+            bestScore = Math.Min(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
     }
   }
 
+  // check if board is empty
   public static bool isEmpty(int x, int y)
   {
     if(board[x,y] == '-')
@@ -36,48 +112,79 @@ class MainClass
     return false;
   }
 
-  public static bool checkForWin()
+  // check if there is a winner or a full board.
+  public static bool hasWon()
   {
-    return checkRow() || checkColumn() || checkDiagonal();
+    if (checkWinner() == "tie")
+    {
+      Console.Write("It was a draw.\n");
+    }
+    else if (checkWinner() != null)
+    {
+      Console.Write($"Player {checkWinner()} wins!\n");
+    }
+    
+    return checkRow() != null || checkColumn() != null || checkDiagonal() != null || isBoardFull();
   }
 
-  public static bool checkRow()
+  // return the name of the winner
+  public static string checkWinner()
+  {
+    if (isBoardFull()) return "tie";
+
+    return checkRow() ?? checkColumn() ?? checkDiagonal();
+  }
+
+  // check rows for winner
+  public static string checkRow()
   {
     for(int i=0; i<3; i++)
     {
       if(board[i,0] != '-' && board[i,0] == board[i,1] && board[i,1] == board[i,2])
       {
-        return true;
+        return Char.ToString(board[i,0]);
       }
     }
-    return false;
+    return null;
   }
 
-  public static bool checkColumn()
+  // check column for winner
+  public static string checkColumn()
   {
     for(int i=0; i<3; i++)
     {
       if(board[0,i] != '-' && board[0,i] == board[1,i] && board[1,i] == board[2,i])
       {
-        return true;
+        return Char.ToString(board[0,i]);
       }
     }
-    return false;
+    return null;
   }
 
-  public static bool checkDiagonal()
+  // check diagonal for winner
+  public static string checkDiagonal()
   {
-    return ((board[0,0] != '-' && board[0,0] == board[1,1] && board[1,1] == board[2,2]) 
-            || (board[0,2] != '-' && board[0,2] == board[1,1] && board[1,1] == board[2,0]));
+    if (board[0,0] != '-' && board[0,0] == board[1,1] && board[1,1] == board[2,2])
+    {
+        return Char.ToString(board[0,0]);
+    } 
+    else if (board[0,2] != '-' && board[0,2] == board[1,1] && board[1,1] == board[2,0])
+    {
+        return Char.ToString(board[0,2]);
+    }
+
+    return null;
   }
 
+  // print tic tac toe board
   public static void printBoard()
   {
-    Console.WriteLine(" {0} | {1} | {2} \n---|---|--- \n {3} | {4} | {5} \n---|---|--- \n {6} | {7} | {8} \n", 
+    Console.WriteLine("\n {0} | {1} | {2} \n---|---|--- \n {3} | {4} | {5} \n---|---|--- \n {6} | {7} | {8} \n", 
       board[0,0], board[0,1], board[0,2], board[1,0], board[1,1], board[1,2], board[2,0], board[2,1], board[2,2]);
   }
 
-  public static bool boardEmpty()
+  // check if board is full
+  public static bool isBoardFull()
   {
     for(int j=0; j<3; j++)
     {
@@ -93,24 +200,40 @@ class MainClass
     return true;
   }
 
+
   public static void Main(string[] args)
   {
-    printBoard();
-    while(checkForWin() != true && boardEmpty() != true)
-    {
-      Console.Write("Enter x: ");
-      int x = int.Parse(Console.ReadLine());
-      Console.Write("Enter y: ");
-      int y = int.Parse(Console.ReadLine());
+    Console.Write("Welcome to Tic Tac Toe.\n");
+    Console.Write("Enter the (x, y) coordinate of where you want to make your move on the board.\n");
 
-      if(isEmpty(x, y))
+    printBoard();
+    // request human to play if board isn't full
+    while(isBoardFull() == false)
+    {
+      Console.Write("Player X's turn\n");
+      Console.Write("Enter x-coordinate: ");
+      int x = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Enter y-coordinate: ");
+      int y = Convert.ToInt32(Console.ReadLine());
+
+      // check if tile is empty
+      try 
       {
-        board[x,y] = 'O';
-        play();
+        if(isEmpty(x, y))
+        {
+          board[x, y] = 'X';
+          // stop program is there is a winner
+          if (hasWon()) System.Environment.Exit(1);
+          play();
+        }
+        else
+        {
+          Console.WriteLine("\nThat square is not empty. Try Again!\n");
+        }
       }
-      else
+      catch(Exception)
       {
-        Console.WriteLine("That square is not empty");
+        Console.WriteLine("\nInvalid move. Please try again!\n");
       }
     }
   }
